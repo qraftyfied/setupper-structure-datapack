f055079a56da59c132a5b8df83566b046a72fca8
qrafty <125701888+qraftyfied@users.noreply.github.com>
2024-03-10 21:29:37 +0100

    Update setupper-structure-datapack.bat
    - Added website support for github wiki
    - Added option to select pack version
    - Added website help for pack version
    - Removed generateted readme.txt in favor of the new github wiki
    - Changed strings, made links clickable

--- setupper-structure-datapack.bat
+++ setupper-structure-datapack.bat
@@ -1,14 +1,29 @@
 @echo off
 
-echo STRUCTURE DATAPACK SETUPPER by qrafty.net/discord QUICK HELP /// REFER TO https://github.com/qraftyfied?tab=repositories /// Structure name has to be the name of the previously generated nbt file. Also the nbt file has to be in the same folder a s this datpack setupper! The namespace is just important for you, if you plan to extend the datapack in future. Refer for further information on YouTube or smth like that. The name of that datapack is completly unnecessary, as it is just the name of the generated zip file. When your prompt to describe the datapack, it generates the pack description in pack.mcmeta. Now it gets important: Biome spawning defines the biome the structure will spawn in. Be sure to use the biome naming from mojang, like windswept_savanna or so. Dont write the "minecraft:" infront of it, it is not necessary. Spacing is self explanatory, the greater the number, the rarer is the structure. Recommendend is anything between 5 and 50. Spereation is basically in one sentence just the same, but make sure the number is littler that the spacing number!!! > "readme.txt"
+echo STRUCTURE DATAPACK SETUPPER by qrafty https://qrafty/discord
 
-echo STRUCTURE DATAPACK SETUP by qrafty (DISCORD)
-echo IF BASIC HELP IS NEEDED, REFER TO THE GENERATED readme.txt!
 
-set /p structurename="INPUT STRUCTURE NAME: "
+set /p needHelp="DO YOU WANT TO OPEN THE GITHUB WIKI BEFORE STARTING? (Y/N): "
+if /i "%needHelp%"=="Y" (
+    start "Open Website" https://github.com/qraftyfied/setupper-structure-datapack/wiki 
+) else (
+    echo CONTINUING...
+)
+
+
+set /p structurename="INPUT GENERATED STRUCTURE NAME: "
 set /p namespace="SET YOUR NAMESPACE: "
 set /p foldername="DEFINE THE NAME OF YOUR DATAPACK: "
 set /p description="DESCRIBE YOUR DATAPACK: "
+
+set /p needHelp="DO YOU NEED TO LOOK UP THE CURRENT PACK VERSION? (Y/N): "
+if /i "%needHelp%"=="Y" (
+    start "Open Website" https://minecraft.wiki/w/Data_pack 
+) else (
+    echo CONTINUING...
+)
+
+set /p packversion="SET PACKVERSION: "
 set /p biome="BIOME SPAWNING: "
 set /p spacing="SPACING: "
 set /p seperation="SEPERATION: "
@@ -28,12 +43,10 @@ mkdir "%foldername%\data\%namespace%\worldgen\structure_set"
 mkdir "%foldername%\data\%namespace%\worldgen\template_pool"
 mkdir "%foldername%\data\%namespace%\worldgen\processor_list"
 
-echo CREATED FILE SYSTEM, CONTINUING WITH JSON!
-
 
 echo {"type":"minecraft:jigsaw","biomes":"#%namespace%:has_structure/%structurename%","step":"surface_structures","spawn_overrides":{},"terrain_adaptation":"beard_thin","start_pool":"%namespace%:%structurename%","size":3,"start_height":{"absolute":0},"project_start_to_heightmap":"WORLD_SURFACE_WG","max_distance_from_center":100,"use_expansion_hack":false} > "%foldername%\data\%namespace%\worldgen\structure\%structurename%.json"
 
-echo {"pack":{"pack_format":26,"description":{"text":"%description%","color":"aqua","hoverEvent":{"action":"show_text","value":"%description%"}}}} > "%foldername%\pack.mcmeta"
+echo {"pack":{"pack_format":%packversion%,"description":{"text":"%description%","color":"aqua","hoverEvent":{"action":"show_text","value":"%description%"}}}} > "%foldername%\pack.mcmeta"
 
 echo {"processors":[{"processor_type":"minecraft:rule","rules":[{"location_predicate":{"predicate_type":"minecraft:block_match","block":"minecraft:sand"},"input_predicate":{"predicate_type":"minecraft:block_match","block":"minecraft:grass_block"},"output_state":{"Name":"minecraft:sand"}},{"location_predicate":{"predicate_type":"minecraft:block_match","block":"minecraft:stone"},"input_predicate":{"predicate_type":"minecraft:block_match","block":"minecraft:grass_block"},"output_state":{"Name":"minecraft:stone"}},{"location_predicate":{"predicate_type":"minecraft:block_match","block":"minecraft:terracotta"},"input_predicate":{"predicate_type":"minecraft:block_match","block":"minecraft:grass_block"},"output_state":{"Name":"minecraft:terracotta"}},{"location_predicate":{"predicate_type":"minecraft:always_true"},"input_predicate":{"predicate_type":"minecraft:random_block_match","block":"minecraft:stone_bricks","probability":0.03},"output_state":{"Name":"minecraft:mossy_stone_bricks"}},{"location_predicate":{"predicate_type":"minecraft:always_true"},"input_predicate":{"predicate_type":"minecraft:random_block_match","block":"minecraft:stone_bricks","probability":0.05},"output_state":{"Name":"minecraft:cracked_stone_bricks"}}]}]} > "%foldername%\data\%namespace%\worldgen\processor_list\%structurename%.json"
 
@@ -49,7 +62,6 @@ move "%structurename%.nbt" "%foldername%\data\%namespace%\structures"
 
 
 
-echo.
-echo DATAPACK SAVED TO THE MINECRAFT SAVES FOLDER! GET SUPPORT: qrafty.net/discord
+echo FINISHED! LOOK FOR THE PACK IN THIS FOLDER. ENJOY MORE CREATIONS BY QRAFTY: https://www.qrafty.net/discord
 
 pause
